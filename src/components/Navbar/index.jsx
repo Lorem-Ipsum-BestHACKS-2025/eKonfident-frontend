df7a6f4e1827c2c6f0c66fe18a30b65d1131d226
Krystian Śliwiński <72706877+Infor-Tech@users.noreply.github.com>
2025-10-11 15:20:27 +0200

    feat: navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
+import style from "./index.module.css";
+
 export default function Navbar() {
   return (
-    <nav>
+    <nav className={style.nav}>
       <Link to="/">Dashboard</Link>
       <Link to="/report">Reports</Link>
       <Link to="/leaderboards">Leaderboard</Link>
+      <button className={style.button}>Button</button>
     </nav>
   );
 }
